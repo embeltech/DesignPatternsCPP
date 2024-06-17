795a07f00d905fd0bf5ff40fcfddc995b488c52e
embeltech <56225547+embeltech@users.noreply.github.com>
2024-06-17 21:40:26 +0530

    Create Creational_Singleton.cpp

---
+++ Creational_Singleton.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class mytest{
+
+  private:
+    int data;
+    static mytest *instance;  
+    mytest(int x):data(x){}  //Private CTOR to avoid 'new' operator object creation
+
+  public:
+    //delete copy CTOR and assignment operator to avoid copy creation
+    mytest(const mytest & ) = delete;
+    void operator=(const mytest & ) = delete;
+    // get instance interface
+    static mytest* getinstance()
+    {
+        if(nullptr == instance)
+        {
+            instance = new mytest(20);
+        }
+        return instance;
+    }
+    void print()
+    {
+        std::cout<<data;
+    }
+
+};
+
+mytest * mytest::instance=nullptr;
+
+int main()
+{
+    mytest *ptr = mytest::getinstance();
+    std::cout<<"Hello World\n";
+    ptr->print();
+   
+    return 0;
+}
